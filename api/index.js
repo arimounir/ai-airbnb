fc1f7861869f9172219ccf9400da3fd8cf40bec2
arimounir <ariel.moguillansky827@comunidadunir.net>
2023-07-08 21:11:47 -0300

    origins v2

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'https://ai-airbnb-gjy0ghto3-arimounir.vercel.app',
+  origin: 'https://ai-airbnb.vercel.app',
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
